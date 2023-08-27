0303238b500373f4adb2dbab71408486658cf884
l4wsss <122654266+l4wsss@users.noreply.github.com>
2023-08-27 10:18:00 +0000

    Deploying to gh-pages from @ l4wsss/goit-js-hw-09@822c48a663f9cfb044d4d1b645f73c2c37b91c54 🚀

---
+++ 01-color-switcher.0f6f9992.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d),d=null,document.body.style.backgroundColor="#ffffff"}));
+//# sourceMappingURL=01-color-switcher.0f6f9992.js.map
